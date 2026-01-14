fc9af74fc593b7f3d6fef7c41dd97abb8bb4c07a
Prathmesh Chopade <prathmeshchopade96@gmail.com>
2026-01-14 11:28:59 +0530

    Java_script

---
+++ Exp_01.js
@@ -0,0 +1,35 @@
+// const declaration
+
+
+// const a="abc@gmail.com"
+// console.log(a)
+
+//scope checking
+
+// let a=10
+// var b=20
+
+// {
+//     let a=200;
+//     var b=300;
+//     console.log(a,b);
+// }
+// console.log(a,b);
+
+
+//variable declaration
+
+let a=100; //number declare
+let b;  //undefined
+let d=null; //null
+let c='Prathmesh';  //string
+let bool=true; //bollean
+console.log(a,b,c,d,bool);
+
+//test type of variable
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(bool))
---
+++ Exp_01_Activity.js
@@ -0,0 +1,41 @@
+//display student info
+let name="Prathmaesh Chopade";
+let roll_no=302;
+let gmail='prathmesh@gmail.com'
+
+document.writeln("student name : "+name);
+document.writeln("student roll no : "+roll_no);
+document.writeln("student gmail : "+gmail);
+
+//number odd even
+let a=1;
+if(a/2==0)
+{
+    document.writeln("Even Number");
+}
+else{
+    document.writeln("Odd Number")
+}
+
+
+//display number 
+
+let array=[1,2,3,4,5];
+for(let i=0;i<array.length;i++)
+{
+    document.writeln(array[i])
+}
+
+
+//student pass fail
+
+
+let c =80;
+if(c >35)
+{
+    document.writeln("Student Pass");
+}
+else
+{
+    document.writeln("Student Fail");
+}
